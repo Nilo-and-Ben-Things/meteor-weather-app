e5c67c84c9353e45cdee11f006453dd8a1c1ebd8
Ben Ingarfield <ben.ingarfield@gmail.com>
2017-11-07 00:19:57 +1100

    get weather from lat long and from postcode input

--- client/main.js
+++ client/main.js
@@ -4,56 +4,91 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-
-
 Template.weather.onCreated(function weatherOnCreated() {
-  this.temperature = new ReactiveVar(0);
+  this.inputcounter = new ReactiveVar(0);
+  this.temp = new ReactiveVar(0);
+  this.temp_min = new ReactiveVar(0);
+  this.temp_max = new ReactiveVar(0);
+  this.description = new ReactiveVar(0);
+  this.postcode = new ReactiveVar('getting current location');
+  this.loc = Geolocation.latLng() || { lat: 0, lng: 0};
+
+  weather.get_data(this, {
+    lat: this.loc.lat,
+    lon: this.loc.lng,
+    units: 'metric',
+  })
 });
 
 Template.weather.helpers({
-  temperature() {
-    return Template.instance().temperature.get();
+  inputcounter() {
+    return Template.instance().inputcounter.get();
+  },
+  temp() {
+    return Template.instance().temp.get();
+  },
+  temp_min() {
+    return Template.instance().temp_min.get();
+  },
+  temp_max() {
+    return Template.instance().temp_max.get();
+  },
+  description() {
+    return Template.instance().description.get();
+  },
+  postcode() {
+    return Template.instance().postcode.get();
+  },
+  loc() {
+    return Geolocation.latLng() || { lat: 0, lng: 0};
   },
 });
 
 Template.weather.events({
-  'click button'(event, instance) {
-    let apiKey = Meteor.settings.public.openweathermapapi;
+  'submit .enter-input'(event, instance) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    instance.inputcounter.set(instance.inputcounter.get() + 1);
+
+    //Get value from the form element
+    const target = event.target;
+    const text = target.input.value;
+
+    //Set postcode
+    instance.postcode.set(text);
+
+    weather.get_data(instance, {
+      zip: instance.postcode.get(),
+      units: 'metric',
+    });
+
+    //Clear form
+    target.input.value = '';
+  },
+});
+
+let weather = {
+  get_data( instance, params ) {
+    let apiKey = Meteor.settings.public.openweathermap;
+    params.appid = apiKey;
 
     HTTP.call('GET', 'http://api.openweathermap.org/data/2.5/weather', {
-        params: {
-          zip: '3029,au',
-          units: 'metric',
-          appid: apiKey
-        }
+        params: params
       },
       (error, result) => {
         if (error) {
           console.log(error);
         }
         else {
-          let temperature = result.data.main.temp;
-          instance.temperature.set(temperature);
+          let data = result.data.main;
+          instance.temp.set(data.temp);
+          instance.temp_min.set(data.temp_min);
+          instance.temp_max.set(data.temp_max);
+          let weather = result.data.weather[0];
+          instance.description.set(weather.description);
         }
       }
     );
-
   },
-});
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { HTTP } from 'meteor/http';
 
 Meteor.startup(() => {
   // code to run on server at startup
